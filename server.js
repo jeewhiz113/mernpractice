d8dcad0615cd0c3b57c9f79519bf2d4ed34df82b
Jee Zhou <jwhiz113@gmail.com>
2020-08-07 19:34:16 -0500

    fixed typos

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');  //allows us to get the request as an object in for example a post request.
-const path = requre('path')
+const path = require('path')
 const items = require('./routes/api/items');
 const app = express();
 
